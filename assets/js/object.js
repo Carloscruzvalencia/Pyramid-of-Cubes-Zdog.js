2babf27e62d8ebbf8cce5f6239446f273537fd59
Carlos Cruz Valencia <a2carcruzinfo@gmail.com>
2023-01-17 22:21:28 +0100

    project update

--- assets/js/object.js
+++ assets/js/object.js
@@ -34,44 +34,43 @@ for (i = 0; i != 11; i++) {
     ps = 10
 }
 function animate() {
-    list1 = ['10', '20', '30', '40', '50', '60', '70', '80', '90']
-    for (i = 0; i != 9; i++) {
-        if (Gcubes.children[list1[i]].translate.z < 131) {
-            Gcubes.children[list1[i]].translate.z += 0.2;
+    FirstLineGroup = [
+        '10','11','12','13','14','15','16','17','18','20',
+        '28','30','38','40','48','50','58','60','68','70',
+        '78','80','88','90','91','92','93','94','95','96',
+        '97','98'
+    ]
+    for (i = 0; i != FirstLineGroup.length; i++) {
+        if (Gcubes.children[FirstLineGroup[i]].translate.z < 131) {
+            Gcubes.children[FirstLineGroup[i]].translate.z += 0.5;
         }
     }
-    list3 = ['91', '92', '93', '94', '95', '96', '97', '98', '99','100']
-    for (i = 0; i != 8; i++) {
-        if (Gcubes.children[list3[i]].translate.z < 131) {
-            Gcubes.children[list3[i]].translate.z += 0.2;
-        }
-    }
+    // list2 = ['21', '31', '41', '51', '61', '71', '81']
+    // for (i = 0; i != 7; i++) {
+    //     if (Gcubes.children[list2[i]].translate.z < 262) {
+    //         Gcubes.children[list2[i]].translate.z += 0.2;
+    //     }
+    // }
+    // list4 = ['82', '83', '84', '85', '86', '87']
+    // for (i = 0; i != 6; i++) {
+    //     if (Gcubes.children[list4[i]].translate.z < 262) {
+    //         Gcubes.children[list4[i]].translate.z += 0.2;
+    //     }
+    // }
 
-    list2 = ['21', '31', '41', '51', '61', '71', '81']
-    for (i = 0; i != 7; i++) {
-        if (Gcubes.children[list2[i]].translate.z < 262) {
-            Gcubes.children[list2[i]].translate.z += 0.2;
-        }
-    }
-    list4 = ['82', '83', '84', '85', '86', '87']
-    for (i = 0; i != 6; i++) {
-        if (Gcubes.children[list4[i]].translate.z < 262) {
-            Gcubes.children[list4[i]].translate.z += 0.2;
-        }
-    }
+    // list5 = ['72', '73', '74', '75', '76']
+    // for (i = 0; i != 5; i++) {
+    //     if (Gcubes.children[list5[i]].translate.z < 393) {
+    //         Gcubes.children[list5[i]].translate.z += 0.2;
+    //     }
+    // }
+    // list5 = ['63', '64', '65']
+    // for (i = 0; i != 3; i++) {
+    //     if (Gcubes.children[list5[i]].translate.z < 524) {
+    //         Gcubes.children[list5[i]].translate.z += 0.2;
+    //     }
+    // }
 
-    list5 = ['72', '73', '74', '75', '76']
-    for (i = 0; i != 5; i++) {
-        if (Gcubes.children[list5[i]].translate.z < 393) {
-            Gcubes.children[list5[i]].translate.z += 0.2;
-        }
-    }
-    list5 = ['63', '64', '65']
-    for (i = 0; i != 3; i++) {
-        if (Gcubes.children[list5[i]].translate.z < 524) {
-            Gcubes.children[list5[i]].translate.z += 0.2;
-        }
-    }
 
     Piramid.updateRenderGraph();
     requestAnimationFrame(animate);
