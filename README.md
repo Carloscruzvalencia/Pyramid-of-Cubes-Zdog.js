dc62a3df2fd729f614de6d5f9b156d595543d8bb
Carlos Cruz Valencia <a2carcruzinfo@gmail.com>
2023-01-19 13:27:30 +0100

    project update

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 - Recrear y animar la siguiente imagen en Zdog.js
 ![inspiracion](inspiration.png))
 
+# Resultado final
+![Resultado final del proyecto](progres/3.gif)
+
+
 <details open>
-<summary>Estado actual del proyecto</summary>
+<summary>Documentacion del proyecto</summary>
 
 ## Mediante un bucle se generan los cubos nesesarios
 ```JavaScript 
@@ -39,4 +43,47 @@ for (i = 0; i != 10; i++) {
 ```
 ## resultado animado 
 ![resulado imagen](progres/2.gif)
+## Creamos una lista que contenga todos los cubos que usaremos
+```JavaScript
+GroupOfCubes = [
+    '10', '11', '12', '13', '14', '15', '16', '17', '18', '20',
+    '28', '30', '38', '40', '48', '50', '58', '60', '68', '70',
+    '78', '80', '88', '90', '91', '92', '93', '94', '95', '96',
+    '97', '98', '21', '22', '23', '24', '25', '26', '27', '31',
+    '37', '41', '47', '51', '57', '61', '67', '71', '77', '81',
+    '82', '83', '84', '85', '86', '87', '32', '33', '34', '35',
+    '36', '42', '46', '52', '56', '62', '66', '72', '73', '74',
+    '75', '76', '43', '44', '45', '53', '55', '63', '64', '65',
+    '54'
+]
+```
+## Cremos un bucle donde animamos todos los cubos
+```JavaScript
+for (i = 0; i != GroupOfCubes.length; i++) {
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 131 & i < 32) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 0.5;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 261 & i >= 32) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 0.99;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 392 & i >= 56) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 1;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 523 & i >= 72) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 1.1;
+        }
+        if (Gcubes.children[54].translate.z < 654) {
+            Gcubes.children[54].translate.z += 0.01;
+        }
+}
+```
+
+## Para terminar animamos toda la figura
+```JavaScript 
+    Piramid.rotate.y += 0.002
+```
+
+# Resultado final
+![Resultado final del proyecto](progres/3.gif)
+
 </details>
--- assets/js/object.js
+++ assets/js/object.js
@@ -33,49 +33,38 @@ for (i = 0; i != 11; i++) {
     }
     ps = 10
 }
+GroupOfCubes = [
+    '10', '11', '12', '13', '14', '15', '16', '17', '18', '20',
+    '28', '30', '38', '40', '48', '50', '58', '60', '68', '70',
+    '78', '80', '88', '90', '91', '92', '93', '94', '95', '96',
+    '97', '98', '21', '22', '23', '24', '25', '26', '27', '31',
+    '37', '41', '47', '51', '57', '61', '67', '71', '77', '81',
+    '82', '83', '84', '85', '86', '87', '32', '33', '34', '35',
+    '36', '42', '46', '52', '56', '62', '66', '72', '73', '74',
+    '75', '76', '43', '44', '45', '53', '55', '63', '64', '65',
+    '54'
+]
 function animate() {
-    FirstLineGroup = [
-        '10','11','12','13','14','15','16','17','18','20',
-        '28','30','38','40','48','50','58','60','68','70',
-        '78','80','88','90','91','92','93','94','95','96',
-        '97','98'
-    ]
-    for (i = 0; i != FirstLineGroup.length; i++) {
-        if (Gcubes.children[FirstLineGroup[i]].translate.z < 131) {
-            Gcubes.children[FirstLineGroup[i]].translate.z += 0.5;
+    for (i = 0; i != GroupOfCubes.length; i++) {
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 131 & i < 32) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 0.5;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 261 & i >= 32) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 0.99;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 392 & i >= 56) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 1;
+        }
+        if (Gcubes.children[GroupOfCubes[i]].translate.z < 523 & i >= 72) {
+            Gcubes.children[GroupOfCubes[i]].translate.z += 1.1;
+        }
+        if (Gcubes.children[54].translate.z < 654) {
+            Gcubes.children[54].translate.z += 0.01;
         }
     }
-    // list2 = ['21', '31', '41', '51', '61', '71', '81']
-    // for (i = 0; i != 7; i++) {
-    //     if (Gcubes.children[list2[i]].translate.z < 262) {
-    //         Gcubes.children[list2[i]].translate.z += 0.2;
-    //     }
-    // }
-    // list4 = ['82', '83', '84', '85', '86', '87']
-    // for (i = 0; i != 6; i++) {
-    //     if (Gcubes.children[list4[i]].translate.z < 262) {
-    //         Gcubes.children[list4[i]].translate.z += 0.2;
-    //     }
-    // }
-
-    // list5 = ['72', '73', '74', '75', '76']
-    // for (i = 0; i != 5; i++) {
-    //     if (Gcubes.children[list5[i]].translate.z < 393) {
-    //         Gcubes.children[list5[i]].translate.z += 0.2;
-    //     }
-    // }
-    // list5 = ['63', '64', '65']
-    // for (i = 0; i != 3; i++) {
-    //     if (Gcubes.children[list5[i]].translate.z < 524) {
-    //         Gcubes.children[list5[i]].translate.z += 0.2;
-    //     }
-    // }
-
-
+    Piramid.rotate.y += 0.002
     Piramid.updateRenderGraph();
     requestAnimationFrame(animate);
-
-
 }
 animate();
 
